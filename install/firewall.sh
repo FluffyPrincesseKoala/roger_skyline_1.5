8d1dff49fc25a75519dd07ca6eac0b8a9840ef1b
Cyril LEMAIRE <cylemair@e1r12p13.42.fr>
2019-11-04 18:49:09 +0100

    lapin

--- install/firewall.sh
+++ install/firewall.sh
@@ -1,3 +1,7 @@
+ufw allow 2222/tcp
+ufw allow 80/tcp
+ufw allow 443
+
 cp -f iptables /etc/network/if-pre-up.d/iptables
 chmod +x /etc/network/if-pre-up.d/iptables
 /etc/network/if-pre-up.d/iptables
